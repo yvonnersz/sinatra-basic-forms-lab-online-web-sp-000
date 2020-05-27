04abdf01d245aba758628ba2aad60327a6ec60aa
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-27 20:45:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    input = params[:name]
+    input = params[:name], params[:age], params[:breed]
     @puppy = Puppy.new(input)
     erb :display_puppy
   end
